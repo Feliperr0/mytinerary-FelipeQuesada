08a513414e771434a6bc98a03a38a2cd26994874
Felipe Quesada <felquev@mail.com>
2025-02-21 17:24:49 -0600

    correciones

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -5,9 +5,7 @@ export default function NotFound() {
         <>
 
 
-       
-            <div>Not Found</div>
-            <p>This is not found</p>
+
             <div className="flex items-center justify-center h-screen bg-gray-900">
                 <div className="border-4 border-yellow-400 bg-opacity-50 p-8 rounded-lg text-center">
                     <p className="text-4xl font-bold text-yellow-400">UNDER CONSTRUCTION</p>
